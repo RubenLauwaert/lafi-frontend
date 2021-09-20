2a54b6725b5fb39bbcdfc7c4934a163bc4b3cb1c
Ruben Lauwaert <43750085+RubenLauwaert@users.noreply.github.com>
2021-09-20 11:00:45 +0200

    RegisterForm Bootstrap

--- src/Components/Forms/LoginForm/LoginForm.tsx
+++ src/Components/Forms/LoginForm/LoginForm.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import styles from '../AuthForm.module.css'
-import googleIcon from '../../../Assets/LoginPage/google_icon.png'
+import React from "react";
+import styles from "../AuthForm.module.css";
+import googleIcon from "../../../Assets/LoginPage/google_icon.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button, Card, Container } from "react-bootstrap";
 
 export const LoginForm = () => {
     return (
         <div className={styles.oauthWrapper}>
-            <div className={styles.oauthContainer}>
-                <h1>Log in</h1>
-                <div className={styles.oauthForm}>
-                    <div className={styles.oauthButton}>
-                        <img src={googleIcon} alt="Google Icon"></img>
-                        <p>Continue With Google</p>
-                    </div>
-                </div>
-            </div>
+            <Container className="mb-3">
+                <Card className="mb-3">
+                    <Card.Title>Log In</Card.Title>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" />
+                        </Form.Group>
+                        <Button type="submit">Continue</Button>
+                    </Form>
+                </Card>
+            </Container>
         </div>
-
-    )
-}
+    );
+};
