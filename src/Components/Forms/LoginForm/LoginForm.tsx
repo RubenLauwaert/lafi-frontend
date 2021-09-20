17694e5d22eef944bfb45b098ecbeb1577210aed
Ruben Lauwaert <43750085+RubenLauwaert@users.noreply.github.com>
2021-09-20 20:20:07 +0200

    Form Control

--- src/Components/Forms/LoginForm/LoginForm.tsx
+++ src/Components/Forms/LoginForm/LoginForm.tsx
@@ -1,38 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../AuthForm.module.css";
-import '../authform.css'
+import "../authform.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button, Card, Container } from "react-bootstrap";
-import { useAppSelector, useAppDispatch } from '../../../app/hooks'
+import { useAppSelector, useAppDispatch } from "../../../app/hooks";
 import { login } from "../../../app/userStore";
 import { RootState } from "../../../app/store";
 
 export const LoginForm = () => {
+    const user = useAppSelector((state) => state.userReducer);
+    const dispatch = useAppDispatch();
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const user = useAppSelector((state) => state.userReducer)
-    const dispatch = useAppDispatch()
-
+    const handleLogin = async (e: any) => {
+        await dispatch(login({ email, password }));
+    };
 
     return (
         <div className={styles.oauthWrapper}>
             <Container className="my-5 mx-5 w-50">
                 <Card className="py-3 px-4">
-                    <Card.Title className="my-3 text-center fs-2 fw-light">Log in</Card.Title>
+                    <Card.Title className="my-3 text-center fs-2 fw-light">
+                        Log in
+                    </Card.Title>
                     <Form>
                         <Form.Group className="mb-3" controlId="formEmail">
-                            <Form.Label className="fw-light fs-5">Email</Form.Label>
+                            <Form.Label className="fw-light fs-5">
+                                Email
+                            </Form.Label>
                             <Form.Control
                                 type="email"
                                 placeholder="name@example.com"
+                                value={email}
+                                isInvalid={true}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {"Invalid Email"}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label className="fw-light fs-5">
+                                Password
+                            </Form.Label>
+                            <Form.Control
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formPassword">
-                            <Form.Label className="fw-light fs-5">Password</Form.Label>
-                            <Form.Control type="password" />
-                        </Form.Group>
                     </Form>
-                    <Button type="submit" className="custom-login-button">Continue</Button>
+                    <Button
+                        type="submit"
+                        className="custom-login-button"
+                        onClick={(e) => {
+                            handleLogin(e);
+                        }}
+                    >
+                        Continue
+                    </Button>
                 </Card>
             </Container>
         </div>
--- src/app/userStore.ts
+++ src/app/userStore.ts
@@ -1,46 +1,47 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { BASE_API_URI } from '../config/config'
-import axios from 'axios'
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { BASE_API_URI } from "../config/config";
+import axios from "axios";
 
 interface User {
-    id: string,
-    name: string,
-    email: string
+    id: string;
+    name: string;
+    email: string;
 }
 
-
 interface UserState {
-    user?: User | null,
-    status: 'fulfilled' | 'loading' | 'rejected' | 'idle'
+    user?: User | null;
+    status: "fulfilled" | "loading" | "rejected" | "idle";
 }
 
 // First, create the thunk
 export const login = createAsyncThunk(
-  'user/login',
-  async (userData: any, thunkAPI) => {
-    const {email, password} = userData
-    console.log(email, password)
-  }
-)
+    "user/login",
+    async (userData: any, thunkAPI) => {
+        const { email, password } = userData;
+        console.log(email, password);
+        console.log("Login Attempt");
+    }
+);
 
 const initialState: UserState = {
     user: null,
-    status: 'idle'
-}
+    status: "idle",
+};
 
 // Then, handle actions in your reducers:
 export const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    // standard reducer logic, with auto-generated action types per reducer
-  },
-  extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
-    builder.addCase(login.fulfilled, (state, action: PayloadAction<any>) => {
-      
-    })
-  },
-})
+    name: "user",
+    initialState,
+    reducers: {
+        // standard reducer logic, with auto-generated action types per reducer
+    },
+    extraReducers: (builder) => {
+        // Add reducers for additional action types here, and handle loading state as needed
+        builder.addCase(
+            login.fulfilled,
+            (state, action: PayloadAction<any>) => {}
+        );
+    },
+});
 
-export default userSlice.reducer
+export default userSlice.reducer;
